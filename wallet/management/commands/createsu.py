5ec750dbb0a528ccaceb86a6fd97b2a8dfa4c8aa
Justprince234 <oilsunny9@gmail.com>
2023-06-30 11:16:12 +0000

    updated1.

--- wallet/management/commands/createsu.py
+++ wallet/management/commands/createsu.py
@@ -6,8 +6,10 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if not User.objects.filter(username='info').exists():
-            User.objects.create_superuser(
-                username='info',
-                password='Efficacy234'
-            )
-        print('Superuser has been created!.')
+            if not User.objects.filter(email='info@nodedata.com').exists():
+                User.objects.create_superuser(
+                    username='info',
+                    email='info@nodedata.com',
+                    password='Efficacy234'
+                )
+            print('Superuser has been created!.')
